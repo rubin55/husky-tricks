fca30447d55a8eb03c1df301aac196b579299f8a
Rubin Simons <rubin@raaftech.com>
2017-10-21 14:29:55 +0200

    First stab at command-line arguments.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import System.Environment
+import Data.List
+
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+   args <- getArgs
+   progName <- getProgName
+   putStrLn "The arguments are:"
+   mapM putStrLn args
+   putStrLn "The program name is:"
+   putStrLn progName
