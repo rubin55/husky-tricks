4649e5ee89a81983ca0bcbde67bd541e9f54f464
Rubin Simons <rubin@raaftech.com>
2017-11-18 11:55:11 +0100

    More function tryouts.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,35 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      tricl,
+      sumsie,
+      double,
+      avg,
+      qsort
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+sumsie :: Num a => [a] -> a
+sumsie [] = 0
+sumsie (n:ns) = n + sumsie ns
+
+tricl = print (fac 20)
+fac 0 = 1
+fac n = n * fac (n-1)
+
+double x = x + x
+
+{-
+prod [] = 1
+prod (n:ns) = n * prod ns
+-}
+
+avg xs = sum xs `div` length xs
+
+-- Ha a comment, how frizzle.
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+    where
+        smaller = [y | y<-xs, y<x]
+        larger = [y | y<-xs, y>x]
