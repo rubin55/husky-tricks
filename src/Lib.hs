c1aed5f630bce228f7dacd4700b365e93d4789b7
Rubin Simons <rubin@raaf.tech>
2019-07-24 18:11:12 +0200

    Pathetic Haskell

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,5 @@
 module Lib
-    ( someFunc, addOne
+    ( someFunc, addOne, factorial, circumference
     ) where
 
 someFunc :: IO ()
@@ -7,3 +7,9 @@ someFunc = putStrLn "Haskell, world!"
 
 addOne :: Int -> Int
 addOne = \x -> x + 1
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+circumference :: Double -> Double
+circumference r = 2 * pi * r
