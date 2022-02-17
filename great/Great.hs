8b724017592b4e3419b4972fb86155e4ef8343b2
Rubin Simons <me@rubin55.org>
2022-02-17 14:41:36 +0100

    More.

--- great/Great.hs
+++ great/Great.hs
@@ -6,6 +6,7 @@ doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
 
+doubleSmallNumber x = if x > 100 then x else x*2
 
 main :: IO ()
 main = printf "This is %d times as great!\n" ((doubleUs 1 2) :: Integer)
